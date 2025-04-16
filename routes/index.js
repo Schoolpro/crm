aed237c65f3da9b713b44ca84b9a6974f1588127
Schoolpro2022 <sakdispatch@gmail.com>
2025-04-15 22:37:01 -0600

    index

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const router = express.Router()
 const contactController = require('../controllers/contactController')
 
+
+// Ruta principal del sitio ("/")
+// Cuando el usuario entra a la página raíz, se le muestra la vista "index.ejs"
+// Esta vista sirve como home o dashboard inicial del CRM
+router.get('/', (req, res) => {
+  res.render('index')
+})
+
+
 // Ruta GET para mostrar los contactos y el formulario
 router.get('/contacts', contactController.showContacts)
 
